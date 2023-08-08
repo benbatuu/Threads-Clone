f0f86dce3d3c9bf7e0c8e3ea46772559ceb18b1c
Batuhan Küçük <bennbatuu@gmail.com>
2023-08-08 13:43:58 +0300

    Finish the Threads Clone using NextJS and Clerk

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,12 +1,14 @@
-import AccountProfile from "@/components/forms/AccountProfile"
-import { currentUser } from "@clerk/nextjs"
+import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-async function Page(){
-    const user = await currentUser();
+import { fetchUser } from "@/lib/actions/user.actions";
+import AccountProfile from "@/components/forms/AccountProfile";
+
+async function Page() {
+  const user = await currentUser();
   if (!user) return null; // to avoid typescript warnings
 
-  const userInfo = await fetchUser(user.id)
+  const userInfo = await fetchUser(user.id);
   if (userInfo?.onboarded) redirect("/");
 
   const userData = {
@@ -18,16 +20,18 @@ async function Page(){
     image: userInfo ? userInfo?.image : user.imageUrl,
   };
 
-    return(
-        <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
-            <h1 className="head-text">OnBoarding</h1>
-            <p className="mt-3 text-base-regular text-light-2">Complete your profile to use Threads</p>
+  return (
+    <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
+      <h1 className='head-text'>Onboarding</h1>
+      <p className='mt-3 text-base-regular text-light-2'>
+        Complete your profile now, to use Threds.
+      </p>
 
-            <section className="mt-9 bg-dark-2 p-10">
-                <AccountProfile user={userData} btnTitle="Continue"/>
-            </section>
-        </main>
-    )
+      <section className='mt-9 bg-dark-2 p-10'>
+        <AccountProfile user={userData} btnTitle='Continue' />
+      </section>
+    </main>
+  );
 }
 
-export default Page
+export default Page;
